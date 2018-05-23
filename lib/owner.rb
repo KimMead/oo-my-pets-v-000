b9411470194599ddc6be6f8f55605c67b0ed1a99
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-23 00:49:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,9 +62,9 @@ class Owner
   end
 
   def list_pets
-    num_fish = @pets[:fish].size
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
+    num_fish = @pets[:fish].count
+    num_dogs = @pets[:dogs].count
+    num_cats = @pets[:cats].count
     return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)"
   end
 
